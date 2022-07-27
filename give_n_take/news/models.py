97e893e706d824cef892bfc67c5699f241c8ff20
shanidmhd <shanidmhd246@gmail.com>
2022-07-27 13:07:41 +0530

    changes added

--- give_n_take/news/models.py
+++ give_n_take/news/models.py
@@ -12,7 +12,7 @@ class News(models.Model):
       description = models.TextField()
       meeting_link = models.CharField(max_length=255)
       news_type = models.ForeignKey(NewsType,on_delete=models.CASCADE,default=None,null=True)
-      news_image = models.ImageField(upload_to='news_images/', max_length=240, blank=True, null=True)
+      news_image = models.ImageField(upload_to='media/news_images/', max_length=240, blank=True, null=True)
       date_added =  models.DateTimeField(blank=True, null=True)
       date_expired =  models.DateTimeField(blank=True, null=True)
       status = models.CharField(max_length=255)
