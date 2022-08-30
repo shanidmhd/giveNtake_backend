d73d6cc654faa3b2dd869e98813d41c0fb743cbc
shanidmhd <shanidmhd246@gmail.com>
2022-08-30 11:49:42 +0530

    changes added

--- give_n_take/user_details/admin.py
+++ give_n_take/user_details/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from user_details.models import *
 # Register your models here.
+
+class UserDetailsAdmin(admin.ModelAdmin):
+    list_display = ['id','username', 'full_name']
+    
+    def full_name(self, obj):
+        return obj.first_name + ' ' + obj.last_name
+
+admin.site.register(UserDetails, UserDetailsAdmin)
