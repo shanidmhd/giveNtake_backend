3863b5e794beb36cad8879af4a5f44163d977233
Kailasvs <cyberonicskailas@gmail.com>
2022-11-29 11:41:07 +0530

    completed qr code validation and expires

--- give_n_take/booking/serializers.py
+++ give_n_take/booking/serializers.py
@@ -1,52 +1,69 @@
-from rest_framework  import serializers
-from . models import Program_model,TicketBooking
-from user_details.models import State,District,UserDetails
-from user_details.serializers  import StateSerializer,DistrictSerializer
+from rest_framework import serializers
+from .models import Program_model, TicketBooking
+from user_details.models import State, District, UserDetails
+from user_details.serializers import StateSerializer, DistrictSerializer
 from register.serializers import admin_booking_serializers
 from drf_writable_nested.serializers import WritableNestedModelSerializer
 import pysnooper
 
 
 class Program_get_Serializer(WritableNestedModelSerializer):
-    fk_admin_id=admin_booking_serializers()
-    fk_state=StateSerializer()
-    fk_district=DistrictSerializer()
-    class Meta :
+    fk_admin_id = admin_booking_serializers()
+    fk_state = StateSerializer()
+    fk_district = DistrictSerializer()
+
+    class Meta:
         model = Program_model
-        fields = '__all__'
-        
+        fields = "__all__"
+
+
 class Program_Serializer(serializers.ModelSerializer):
-    price =serializers.IntegerField(required=True)
-    class Meta :
+    price = serializers.IntegerField(required=True)
+
+    class Meta:
         model = Program_model
-        fields = '__all__'
+        fields = "__all__"
+
 
 class Ticket_Booking_serializer(serializers.ModelSerializer):
     no_of_seats = serializers.IntegerField(required=True, min_value=5)
-    fk_program =serializers.IntegerField(required=True)
-    class Meta :
+    qr_random_num = serializers.CharField(write_only=True,required=False)
+    qr_image_scanned = serializers.BooleanField(write_only=True)
+    class Meta:
         model = TicketBooking
-        fields = '__all__'
-        
-    def create(self,validated_data):
+        fields = "__all__"
+
+    def create(self, validated_data):
         ticket = TicketBooking.objects.create(**validated_data)
         return ticket
-    
+
     def to_representation(self, instance):
-        res=super().to_representation(self.instance)
-        res['fk_user_id']=UserDetails.objects.filter(id=res['fk_user_id']).values('id','username')
-        res['fk_program']=Program_model.objects.filter(id=res['fk_program']).values('id','program_name','venue','date','available_seats')
+        res = super().to_representation(self.instance)
+        res["fk_user_id"] = UserDetails.objects.filter(id=res["fk_user_id"]).values(
+            "id", "username"
+        )
+        print(res["fk_program"],'open')
+        res["fk_program"] = Program_model.objects.filter(id=res["fk_program"]).values(
+            "id", "program_name", "venue", "date", "available_seats","fk_admin_id__id"
+        )
         return res
-    
+
 
 class ticket_get_serializer(serializers.ModelSerializer):
-    
-    
-    class Meta :
+    qr_random_num = serializers.CharField(write_only=True)
+    qr_image_scanned = serializers.BooleanField(write_only=True)
+    class Meta:
         model = TicketBooking
-        fields = '__all__'
-        
-    def create(self,validated_data):
+        fields = "__all__"
+
+    def create(self, validated_data):
         ticket = TicketBooking.objects.create(**validated_data)
         return ticket
-    
+
+
+class qr_vallidate_serialier(serializers.ModelSerializer):
+   
+    class Meta:
+        model = TicketBooking
+        fields = "__all__"
+
--- give_n_take/booking/urls.py
+++ give_n_take/booking/urls.py
@@ -1,14 +1,13 @@
-from django.urls import path,include
+from django.urls import path, include
 from .views import *
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('details', ProgramAPI)
-router.register('booking', TicketBooking_API)
-
+router.register("details", ProgramAPI)
+router.register("booking", TicketBooking_API)
 
 
 urlpatterns = [
-    path('', include(router.urls)),
-    
-    ]
+    path("", include(router.urls)),
+    path('qr/<str:random>/',invalid_qrcode)
+]
