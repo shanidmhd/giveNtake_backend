c2ebd1d583d8ee81788f6c116fc3277c992e3adf
Kailasvs <cyberonicskailas@gmail.com>
2023-03-09 13:52:49 +0530

    solved issue in news image

--- give_n_take/news/serializers.py
+++ give_n_take/news/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import *
-
+from django.conf import settings
 class NewsTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = NewsType
@@ -16,6 +16,12 @@ class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
         fields = ['id','title','description','meeting_link','news_type','committe_type','news_image','date_added','date_expired','status','created_by','modified_by','date_modified','district_region','state_region','panchayath_region']
+        
+    def to_representation(self, instance):
+        rep= super().to_representation(instance)
+        if rep['news_image']:
+            rep['news_image']=settings.HOST_ADDRESS+settings.MEDIA_URL+rep['news_image']
+        return rep
         
 
     def create(self,validated_data):
