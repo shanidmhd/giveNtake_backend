493fc4a90fdc585a6fcb21a449e9d41cee8122b3
Kailasvs <cyberonicskailas@gmail.com>
2023-03-09 15:13:45 +0530

    permisison

--- give_n_take/register/permission.py
+++ give_n_take/register/permission.py
@@ -44,16 +44,15 @@ class IsNews(BasePermission):
 class IsMeeting(BasePermission):
   
     def has_permission(self, request, view):
-
-        if admin_model.objects.filter(user_id__id=request.user.id).first():
+        if request.user.is_superuser :
+            return True
+        elif admin_model.objects.filter(user_id__id=request.user.id).first():
             if 'meeting' in roles_users(request):
                 return True
             else :
                 return False
-        elif request.user.is_superuser:
-                return True
         else :
-                return False
+            return False
 
 class Iscommitteeadmin(BasePermission):
     def has_permission(self, request, view):
